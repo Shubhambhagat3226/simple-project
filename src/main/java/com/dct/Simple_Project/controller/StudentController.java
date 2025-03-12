96b5d7f5f17dfb149d27fc7f030e60dd942a7e61
Shubhambhagat3226 <shubhambhagat3220@gmail.com>
2025-03-12 20:20:12 +0530

    github page link

--- src/main/java/com/dct/Simple_Project/controller/StudentController.java
+++ src/main/java/com/dct/Simple_Project/controller/StudentController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://127.0.0.1:5500")
+@CrossOrigin(origins = "https://shubhambhagat3226.github.io/simple-page")
 public class StudentController {
 
     private final StudentService service;
